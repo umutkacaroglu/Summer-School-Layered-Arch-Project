89287412945bc1eaf556a2e2a05476fe9df760db
umutkacaroglu <66180124+umutkacaroglu@users.noreply.github.com>
2021-03-07 19:38:07 +0300

    Data Access Layer Katmanına Ait Classlar Ve Operasyonlar Yazıldı.

---
+++ DataAcessLayer/ConnectorClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataAccessLayer
+{
+    class ConnectorClass
+    {
+        //SQL Bağlantısını public olarak tanımlanan nesne ile istedğimiz yerde sağlayacağız.
+        public static SqlConnection connector = new SqlConnection("Data Source=DESKTOP-2IQELU5\\UMUTKACAROGLU271;Initial Catalog=Dbo_YazOkulu;Integrated Security=True");
+
+    }
+}
---
+++ DataAcessLayer/DataLayerStudent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using EntityLayer;
+
+
+namespace DataAccessLayer
+{
+    public class DataLayerStudent
+    {
+        public static int AddStudent(EntityStudent parameters)
+        {
+            SqlCommand InsertStudent = new SqlCommand("insert into Tbl_Ogrenci(Ogrenci_Ad,Ogrenci_Soyad,Ogrenci_Sifre,Ogranci_Mail,Ogrenci_Numara) values(@OgrenciAdi,@OgrenciSoyad,@OgrenciSifre,@OgrenciMail,@OgrenciNumara)", ConnectorClass.connector);
+            if (InsertStudent.Connection.State != ConnectionState.Open)
+                InsertStudent.Connection.Open();
+            InsertStudent.Parameters.AddWithValue("@OgrenciAdi", parameters.Name);
+            InsertStudent.Parameters.AddWithValue("@OgrenciSoyad", parameters.Surname);
+            InsertStudent.Parameters.AddWithValue("@OgrenciSifre", parameters.Password);
+            InsertStudent.Parameters.AddWithValue("@OgrenciMail", parameters.Mail);
+            InsertStudent.Parameters.AddWithValue("@OgrenciNumara", parameters.Number);
+            return InsertStudent.ExecuteNonQuery();
+
+        }
+    }
+}
