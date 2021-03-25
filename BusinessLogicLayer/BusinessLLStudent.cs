ed3cf4ae4bf4f6d1ca61e179e90a141c32ae9d50
umutkacaroglu <66180124+umutkacaroglu@users.noreply.github.com>
2021-03-25 20:47:43 +0300

    Business Logic Layer Vol.2

--- BusinessLogicLayer/BusinessLLStudent.cs
+++ BusinessLogicLayer/BusinessLLStudent.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using EntityLayer;
 using DataAccessLayer;
-namespace BusinessLogicLayer
+using EntityLayer;
+
+namespace BussinesLogicLayer
 {
     public class BusinessLLStudent
     {
+        public static int AddStudent_BusinessLogic(EntityStudent parameters)
+        {
+            if(parameters.Name!=null && parameters.Surname!=null && parameters.Number!=null && parameters.Password!=null && parameters.Mail != null)
+            {
+                return DataLayerStudent.AddStudent(parameters);
+            }
+            return -1;
+        }
+        public List<EntityStudent> Business_Logic_Layer_List()
+        {
+            return DataLayerStudent.StudentList();
+        }
     }
 }
