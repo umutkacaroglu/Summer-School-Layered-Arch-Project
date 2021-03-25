f7f61444fcb41c77765b6719d2a5224fcbabd0a4
umutkacaroglu <66180124+umutkacaroglu@users.noreply.github.com>
2021-03-25 20:48:38 +0300

    Data Acces Layer Vol.2

--- DataAcessLayer/DataLayerStudent.cs
+++ DataAcessLayer/DataLayerStudent.cs
@@ -23,7 +23,27 @@ namespace DataAccessLayer
             InsertStudent.Parameters.AddWithValue("@OgrenciMail", parameters.Mail);
             InsertStudent.Parameters.AddWithValue("@OgrenciNumara", parameters.Number);
             return InsertStudent.ExecuteNonQuery();
-
+        }
+        public static List<EntityStudent> StudentList()
+        {
+            List<EntityStudent> Value = new List<EntityStudent>();
+            SqlCommand komut = new SqlCommand("Select * from Tbl_Ogrenci", ConnectorClass.connector);
+            if (komut.Connection.State != ConnectionState.Open)
+                komut.Connection.Open();
+            SqlDataReader Reader = komut.ExecuteReader();
+            while (Reader.Read())
+            {
+                EntityStudent ent = new EntityStudent();
+                ent.StudentID = Convert.ToInt32(Reader["Ogrenci_ID"].ToString());
+                ent.Name =Reader["Ogrenci_Ad"].ToString();
+                ent.Surname =Reader["Ogrenci_Soyad"].ToString();
+                ent.Password =Reader["Ogrenci_Sifre"].ToString();
+                ent.Mail = Reader["Ogrenci_Mail"].ToString();
+                ent.Balance =Convert.ToDouble(Reader["Ogrenci_Bakiye"].ToString());
+                Value.Add(ent); //Ent Varlığından gelen değerlieri Value adındaki listeye ekle.
+            }
+            Reader.Close();
+            return Value;
         }
     }
 }
